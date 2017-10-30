65db3ee3026be55497b39025ea577c074fe5ea02
KenilMehta <mehtakenil732@gmail.com>
2017-10-30 22:11:00 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/admin/chat_application/SignupLoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.chat_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_login);
+    }
+}
